860c0a8310eba8f8bd300c9a01b4f3a205205d04
Crowdbotics <team@crowdbotics.com>
2022-09-22 18:30:25 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "bitcoininvestment_37115",
+  url: "https://bitcoininvestment_37115.botics.co",
+  api: "https://bitcoininvestment_37115.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
